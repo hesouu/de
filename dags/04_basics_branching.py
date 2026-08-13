e11d2f7240c7086dfcd26a28f9b0abde1e92ce7e
hesouu <hikari251025@gmail.com>
2026-08-13 10:46:20 +0900

    feat: mysql 서비스 추가

--- dags/04_basics_branching.py
+++ dags/04_basics_branching.py
@@ -1,50 +1,77 @@
 '''
-- airflow 내부 정보 접근, 출력시 jinja 활용, 내부 정보 접근시 macro 활용
-- 콜백함수 내부 kwargs를 인자를 통해 접근, 기타 일반적인 상황 jinja를 이용하여 접근
+- 파이썬 오퍼레이터와 연결된 콜백 함수 내부 연산의 결과로 조건부로 task를 선택하여 진행
+- 의존성 컨트롤, 조건부 task 수행 -> 브런치(가지치기)
+- 의존성 구성에서 여러 시나리오 작성
 '''
 # 1. 모듈
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator # 조건부 선택
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.trigger_rule import TriggerRule # 성공, 실패, 등등 조건 설정
 from datetime import datetime, timedelta
 import logging
 import pendulum
+import random
 
 # 2. 전역변수
 KST = pendulum.timezone("Asia/Seoul")
 
+# 4-1. 콜백함수
+def _branch_cb(**kwargs):
+  '''
+    분기 처리 -> 특정 task로 다음 수행 지정 -> task_id값을 반환
+  '''
+  if random.choice([True, False]):
+    logging.info("task process 분기")
+    return "process" # 다음에 수행할 task_id값을 반환
+  else:
+    logging.info("task skip 분기")
+    return "skip"
+  pass
+
+def _process_cb(**kwargs):
+  logging.info("task process 수행")
+  pass
 
 # 3. DAG
 with DAG( 
-  dag_id      = "03_basics_context_jinja",
-  description = "macro을 이용하여 context 접근, jinja를 통해 표현",
+  dag_id      = "04_basics_branching",
+  description = "분기 처리, 선택적 TASK 구동",
   default_args= {
     "owner"           : "aic-de1-admin",    
     "retries"         : 1,
     "retry_delay"     : timedelta(minutes=1)
   },
-  # 매일 오전 9시 00분에 스케줄 작동
-  schedule_interval = "0 9 * * *", # cron 방식으로 표기 (분, 시, 일, 월, 주)
-  # 수행 시작 시간 서울 시간대 타임존 조정
+  schedule_interval = "@daily",  
   start_date  = pendulum.datetime( 2026,6,29, tz=KST ),
   catchup     = False,
-  tags        = ['macro', 'context', 'jinja']
+  tags        = ['branch', 'trigger_rule']
 ) as dag:
-
   # 4. 오퍼레이터를 이용하여 task를 정의
-  t1 = BashOperator(
-    task_id         = "jinja_used_task",
-    bash_command    = "echo 'DAG의 t1 task 수행시간 {{ ds }}, {{ ti }}' "
+  task_start   = EmptyOperator(
+    task_id = "start"
   )
-  t2 = BashOperator(
-    task_id         = "jinja_macros_task",
-    # macro를 통해서 준비된 함수 활용
-    bash_command    = "echo 'DAG의 t1 task일주일전 수행시간(임시) {{ macros.ds_add(ds, -7) }}, 램덤 {{ macros.random() }}' "
+  task_branch  = BranchPythonOperator(
+    task_id = "branch",
+    python_callable = _branch_cb
   )
-  t3 = PythonOperator(
-    task_id         = "jinja_python_task",
-    python_callable = _print
+  task_process = PythonOperator(
+    task_id = "process",
+    python_callable = _process_cb
   )
-
-  # 5. 의존성(수행순서)
-  t1 >> t2 >> t3
+  task_skip    = EmptyOperator(
+    task_id = "skip"
+  )
+  task_end     = EmptyOperator(
+    task_id = "end",
+    # 분기 처리를 수행 => 진행되지 않은 task 가 존재 => 성공/엔딩의 기준을 설정해야함
+    # task 전체 수행에 대한 조건 부여
+    # 실패 x, 최소 1개는 성공해야함 => 완료    
+    trigger_rule = TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
+  )
+  
+  # 5. 의존성(수행순서), 시나리오별 준비 (사전 요구사항에 의해 결정)
+  #    기준 스케줄러, cron과 차별성 => 시나리오 구성 가능
+  task_start  >> task_branch
+  task_branch >> task_process  >> task_end
+  task_branch >> task_skip     >> task_end
